b613d954de4d7269423badf0aeba2ff978d4b237
ljs7143 <junseo27643779@gmail.com>
2024-10-06 21:44:14 +0900

    complete 193(feat: 글 등록 시, 403권한 거부 확인)

--- src/main/java/com/jojoldu/com/freelecspringboot2webservice/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/com/freelecspringboot2webservice/config/auth/SecurityConfig.java
@@ -2,15 +2,17 @@ package com.jojoldu.com.freelecspringboot2webservice.config.auth;
 
 import com.jojoldu.com.freelecspringboot2webservice.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
